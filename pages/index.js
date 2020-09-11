f0d6fc5f343f56e652570f600c168fef3c76dc6c
Vjeko <vjeko.babic@matmatch.com>
2020-09-11 09:50:26 +0200

    Adding meta description

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Franu Küchen</title>
+        <meta name="description" content="Bestellen Sie Ihre Traumküche mit nur wenigen Klicks" />
+        <meta name="keywords" content="Traumküche, Küche" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
 
