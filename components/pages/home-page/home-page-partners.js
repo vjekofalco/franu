722f9c5b534336ac4d0e192e2a2c1aae60d0e7c7
Vjeko <vjeko.babic@matmatch.com>
2021-03-10 15:14:27 +0100

    New partner logos

--- components/pages/home-page/home-page-partners.js
+++ components/pages/home-page/home-page-partners.js
@@ -4,14 +4,15 @@ import styled from 'styled-components'
 import { Section } from '../../common/section'
 import { brown, white } from '../../../styles/colors'
 import { HeadlineSecondary, Text } from '../../common/text'
+import { ImageOptimized } from '../../image/image-optimized'
 import { mediaBreakpointDown } from '../../../styles/breakpoionts'
 import { SeparationLineDefault } from '../../common/separation-line'
 import { baseUnit, PAGE_CONTENT_WIDE_WIDTH } from '../../../common/constants'
 
-import Bosch from '../../../images/partners/bosch-logo.svg?sprite'
-import Eeger from '../../../images/partners/eeger-logo.svg?sprite'
-import Saturn from '../../../images/partners/saturn-logo.svg?sprite'
-import MediaMarkt from '../../../images/partners/media-markt-logo.svg?sprite'
+import Bosch from '../../../images/partners/bosch-logo.png'
+import Gorenje from '../../../images/partners/gorenje-logo.png'
+import Deante from '../../../images/partners/deante-logo.png'
+import Blum from '../../../images/partners/blum-logo.png'
 
 const HomePagePartnersSection = styled(Section)`
   background-color: ${white};
@@ -23,18 +24,23 @@ const HomePagePartnersSection = styled(Section)`
 const HomePagePartnersWrapper = styled.div`
   display: flex;
   margin: 0 auto;
-  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
-  align-items: center;
+  align-content: center;
   max-width: ${PAGE_CONTENT_WIDE_WIDTH + 2 * baseUnit}px;
+
+  max-height: 60px;
+
+  img {
+    max-height: 100%;
+  }
   
   @media ${mediaBreakpointDown.s} {
     overflow-x: auto;
     flex-wrap: nowrap;
   }
   
-  > svg {
+  > img, picture {
     width: 20%;
     
     @media ${mediaBreakpointDown.s} {
@@ -47,16 +53,21 @@ const HomePagePartnersWrapper = styled.div`
   }
 `
 
+const HomePagePartnersImageWrapper = styled.div`
+  display: flex;
+  
+`
+
 export const HomePagePartners = ({ f }) => {
     return (<HomePagePartnersSection>
         <HeadlineSecondary color={brown} center marginBottom={1}>{f('partners.title')}</HeadlineSecondary>
         <SeparationLineDefault center maxWidth={350} marginBottom={1}/>
         <Text size={1.5} center marginBottom={3}>{f('partners.subTitle')}</Text>
         <HomePagePartnersWrapper>
-            <Bosch/>
-            <MediaMarkt/>
-            <Saturn/>
-            <Eeger/>
+            <ImageOptimized relativeUrl={'partners/bosch-logo.png'} alt={'bosch logo'}/>
+            <ImageOptimized relativeUrl={'partners/blum-logo.png'} alt={'blum logo'}/>
+            <ImageOptimized relativeUrl={'partners/gorenje-logo.png'} alt={'gorenje logo'}/>
+            <ImageOptimized relativeUrl={'partners/deante-logo.png'} alt={'deante logo'}/>
         </HomePagePartnersWrapper>
     </HomePagePartnersSection>)
 }
