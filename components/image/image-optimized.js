bc4131c801a2824dc53ed002db93075c66b1a632
vjekofalco <babicvjeko@gmail.com>
2022-01-31 10:10:46 +0100

    LCP index page optimizes

--- components/image/image-optimized.js
+++ components/image/image-optimized.js
@@ -1,25 +1,41 @@
-export const ImageOptimized = ({ relativeUrl, alt, url }) => {
-    if (!url && !relativeUrl) {
-        return (<picture>
-            <source srcSet={require('../../images/placeholder-image.png?webp')} type="image/webp" />
-            <source srcSet={require('../../images/placeholder-image.png')} />
-            <img src={require('../../images/placeholder-image.png')} alt={alt || ''}/>
-        </picture>)
-    }
-
-    if (url) {
-        return (<img src={url} alt={alt || ''}/>)
-    }
-
+export const ImageOptimized = ({ relativeUrl, alt, url, width, height }) => {
+  if (!url && !relativeUrl) {
     return (
-        <picture>
-            <source srcSet={require('../../images/' + relativeUrl + '?webp')} type="image/webp" />
-            <source srcSet={require('../../images/' + relativeUrl)} />
-            <img src={require('../../images/' + relativeUrl)} alt={alt || ''}/>
-        </picture>
+      <picture>
+        <source
+          srcSet={require("../../images/placeholder-image.png?webp")}
+          type="image/webp"
+        />
+        <source srcSet={require("../../images/placeholder-image.png")} />
+        <img
+          src={require("../../images/placeholder-image.png")}
+          alt={alt || ""}
+        />
+      </picture>
     )
+  }
+
+  if (url) {
+    return <img src={url} alt={alt || ""} width={width} height={height} />
+  }
+
+  return (
+    <picture>
+      <source
+        srcSet={require("../../images/" + relativeUrl + "?webp")}
+        type="image/webp"
+      />
+      <source srcSet={require("../../images/" + relativeUrl)} />
+      <img
+        src={require("../../images/" + relativeUrl)}
+        alt={alt || ""}
+        width={width}
+        height={height}
+      />
+    </picture>
+  )
 }
 
 export const ImageOptimizedSvg = ({ relativeUrl, alt }) => {
-    return (<img src={require('../../images/' + relativeUrl)} alt={alt || ''}/>)
+  return <img src={require("../../images/" + relativeUrl)} alt={alt || ""} />
 }
