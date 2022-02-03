6b463a5f7461a67f650cea840d449af2439e2e02
vjekofalco <babicvjeko@gmail.com>
2022-02-03 20:07:43 +0100

    Contact page to mibile

--- components/header/mobile-header-menu.js
+++ components/header/mobile-header-menu.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components'
-import React, { useEffect } from 'react'
+import styled from "styled-components"
+import React, { useEffect } from "react"
 
-import { HeaderLink, HeaderLinkLike } from './header-link'
-import {baseUnit} from "../../common/constants";
+import { HeaderLink, HeaderLinkLike } from "./header-link"
+import { baseUnit } from "../../common/constants"
 
 const MobileHeaderMenuWrapper = styled.div`
   position: relative;
@@ -16,39 +16,60 @@ const MobileHeaderOverlay = styled.div`
   overflow-y: scroll;
   position: absolute;
   height: calc(100vh - 65px);
-  background-color: rgb(224,224,224, 0.8);
+  background-color: rgb(224, 224, 224, 0.8);
 `
 
 const MobileHeaderLinkWrapper = styled.div`
   text-align: right;
-  padding: ${2 * baseUnit}px ${baseUnit}px 0px ${baseUnit}px
+  padding: ${2 * baseUnit}px ${baseUnit}px 0px ${baseUnit}px;
 `
 
-export const MobileHeaderMenu = ({slideOnHomePage, closeOverlay, f}) => {
-    useEffect(() => {
-        document.body.classList.add('body-noscroll')
+export const MobileHeaderMenu = ({ slideOnHomePage, closeOverlay, f }) => {
+  useEffect(() => {
+    document.body.classList.add("body-noscroll")
 
-        return () => document.body.classList.remove('body-noscroll')
-    }, [])
+    return () => document.body.classList.remove("body-noscroll")
+  }, [])
 
-    return (<MobileHeaderMenuWrapper>
-        <MobileHeaderOverlay>
-            <MobileHeaderLinkWrapper>
-                <HeaderLink onClick={() => closeOverlay()} block href={'/'} text={f('common.home')}/>
-            </MobileHeaderLinkWrapper>
-            <MobileHeaderLinkWrapper>
-                <HeaderLink onClick={() => closeOverlay()} block href={'/about-us'} text={f('common.aboutUs')}/>
-            </MobileHeaderLinkWrapper>
-            <MobileHeaderLinkWrapper>
-                <HeaderLinkLike block onClick={() => {
-                    closeOverlay()
-                    slideOnHomePage('how-it-works')}} >{f('common.howItWorks')}</HeaderLinkLike>
-            </MobileHeaderLinkWrapper>
-            <MobileHeaderLinkWrapper>
-                <HeaderLinkLike block onClick={() => {
-                    closeOverlay()
-                    slideOnHomePage('contact-us-form')}} >{f('common.contact')}</HeaderLinkLike>
-            </MobileHeaderLinkWrapper>
-        </MobileHeaderOverlay>
-    </MobileHeaderMenuWrapper>)
+  return (
+    <MobileHeaderMenuWrapper>
+      <MobileHeaderOverlay>
+        <MobileHeaderLinkWrapper>
+          <HeaderLink
+            onClick={() => closeOverlay()}
+            block
+            href={"/"}
+            text={f("common.home")}
+          />
+        </MobileHeaderLinkWrapper>
+        <MobileHeaderLinkWrapper>
+          <HeaderLink
+            onClick={() => closeOverlay()}
+            block
+            href={"/about-us"}
+            text={f("common.aboutUs")}
+          />
+        </MobileHeaderLinkWrapper>
+        <MobileHeaderLinkWrapper>
+          <HeaderLinkLike
+            block
+            onClick={() => {
+              closeOverlay()
+              slideOnHomePage("how-it-works")
+            }}
+          >
+            {f("common.howItWorks")}
+          </HeaderLinkLike>
+        </MobileHeaderLinkWrapper>
+        <MobileHeaderLinkWrapper>
+          <HeaderLink
+            onClick={() => closeOverlay()}
+            block
+            href={"/contact"}
+            text={f("common.contact")}
+          />
+        </MobileHeaderLinkWrapper>
+      </MobileHeaderOverlay>
+    </MobileHeaderMenuWrapper>
+  )
 }
