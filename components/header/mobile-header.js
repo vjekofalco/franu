f9cd908fe1bd0bd1269bc4c63b7534d9db8ee81e
vjekofalco <babicvjeko@gmail.com>
2022-02-18 12:41:00 +0100

    Optimizing

--- components/header/mobile-header.js
+++ components/header/mobile-header.js
@@ -1,13 +1,14 @@
-import styled from 'styled-components'
-import React, { useState } from 'react'
+import Link from "next/link"
+import styled from "styled-components"
+import React, { useState } from "react"
 
-import { baseUnit } from '../../common/constants'
-import { LanguagePicker } from '../language-picker'
-import { MobileHeaderMenu } from './mobile-header-menu'
-import { ImageOptimizedSvg } from '../image/image-optimized'
-import { mediaBreakpointUp } from '../../styles/breakpoionts'
+import { baseUnit } from "../../common/constants"
+import { LanguagePicker } from "../language-picker"
+import { MobileHeaderMenu } from "./mobile-header-menu"
+import { ImageOptimizedSvg } from "../image/image-optimized"
+import { mediaBreakpointUp } from "../../styles/breakpoionts"
 
-import Menu from '../../images/icons/common/menu.svg?sprite'
+import Menu from "../../images/icons/common/menu.svg?sprite"
 
 const MobileHeaderOuterWrapper = styled.div`
   @media ${mediaBreakpointUp.l} {
@@ -36,24 +37,43 @@ const MenuIconWrapper = styled.div`
 `
 
 export const MobileHeader = ({ slideOnHomePage, f }) => {
-    const [overlayOpened, setOverlayOpened] = useState(false)
+  const [overlayOpened, setOverlayOpened] = useState(false)
 
-    const closeOverlay = () => {
-        setOverlayOpened(false)
-    }
+  const closeOverlay = () => {
+    setOverlayOpened(false)
+  }
 
-    return (<MobileHeaderOuterWrapper>
-        <MobileHeaderWrapper>
-            <MobileHeaderLogoArea>
-                <ImageOptimizedSvg relativeUrl={'logos/franu-text-colored.svg'} alt="Franu kuchen logo"/>
-            </MobileHeaderLogoArea>
-            <MobileMenuArea>
-                <LanguagePicker/>
-                <MenuIconWrapper>
-                    <Menu width={30} height={30} onClick={() => setOverlayOpened(!overlayOpened)}/>
-                </MenuIconWrapper>
-            </MobileMenuArea>
-        </MobileHeaderWrapper>
-        {overlayOpened && <MobileHeaderMenu slideOnHomePage={slideOnHomePage} closeOverlay={closeOverlay} f={f}/>}
-    </MobileHeaderOuterWrapper>)
+  return (
+    <MobileHeaderOuterWrapper>
+      <MobileHeaderWrapper>
+        <MobileHeaderLogoArea>
+          <Link href={"/"} passhref>
+            <a>
+              <ImageOptimizedSvg
+                relativeUrl={"logos/franu-text-colored.svg"}
+                alt="Franu kuchen logo"
+              />
+            </a>
+          </Link>
+        </MobileHeaderLogoArea>
+        <MobileMenuArea>
+          <LanguagePicker />
+          <MenuIconWrapper>
+            <Menu
+              width={30}
+              height={30}
+              onClick={() => setOverlayOpened(!overlayOpened)}
+            />
+          </MenuIconWrapper>
+        </MobileMenuArea>
+      </MobileHeaderWrapper>
+      {overlayOpened && (
+        <MobileHeaderMenu
+          slideOnHomePage={slideOnHomePage}
+          closeOverlay={closeOverlay}
+          f={f}
+        />
+      )}
+    </MobileHeaderOuterWrapper>
+  )
 }
